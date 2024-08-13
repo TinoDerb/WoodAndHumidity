24557acfd39f9434e39d7332b5e7f1fed1d42f44
Tino D <81869266+TinoDerb@users.noreply.github.com>
2024-08-13 08:45:06 +0200

    Add files via upload

---
+++ calc_wood_moisture_from_resistance.py
@@ -0,0 +1,13 @@
+def convertVwToMC(R, T = np.nan):
+    '''
+    Inputs: resistance of the wood piece in Megaohm
+    returns the MC of wood in %
+    Reference: https://www.diva-portal.org/smash/get/diva2:1080039/FULLTEXT01.pdf
+    Please consider adjusting the parameters of the curve for your specific wood species
+    '''
+    if np.isnan(T):
+        MC = -np.log((np.log(R)+2.79)/26)/0.0845 # convert the wood resistance into moisture content
+        return round(MC,2) # return two-decimal rounded results
+    else:
+        MC_C = (MC+0.567-0.026*(T+2.8)+0.000051*T**2)/(0.881*1.0056**(T+2.8)) # correct MC
+        return round(MC_C,2)
